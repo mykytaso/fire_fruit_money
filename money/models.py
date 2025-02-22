e7ca65fcf2ef09b821bad550ee868ad9a32a7461
mykytaso <mykyta.soloviov@gmail.com>
2025-02-22 15:58:51 -0500

    Fix PEP8 in users/models.py

--- money/models.py
+++ money/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from users.models import Family
 
 
-
 class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    family = models.ForeignKey(Family, on_delete=models.CASCADE, related_name="categories")
+    family = models.ForeignKey(
+        Family, on_delete=models.CASCADE, related_name="categories"
+    )
     title = models.CharField(max_length=100)
     color = models.CharField(max_length=6)
     icon = models.CharField(max_length=255)
     limit = models.DecimalField(max_digits=12, decimal_places=2)
-
 
     def __str__(self):
         return self.title
@@ -30,7 +30,6 @@ class Tag(models.Model):
         Category, on_delete=models.CASCADE, related_name="tags"
     )
 
-
     def __str__(self):
         return self.title
 
@@ -39,7 +38,9 @@ class Expense(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    family = models.ForeignKey(Family, on_delete=models.CASCADE, related_name="expenses")
+    family = models.ForeignKey(
+        Family, on_delete=models.CASCADE, related_name="expenses"
+    )
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="expenses"
     )
@@ -48,7 +49,6 @@ class Expense(models.Model):
     )
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date_time = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return f"{self.category}: {self.amount} at {self.date_time.strftime('%Y-%m-%d %H:%M:%S')}"
