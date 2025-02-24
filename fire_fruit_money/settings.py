86d12653a7094a68219c36e85581aa3774cda028
mykytaso <mykyta.soloviov@gmail.com>
2025-02-24 14:51:54 -0500

    Set ACCESS_TOKEN_LIFETIME to 30 days

--- fire_fruit_money/settings.py
+++ fire_fruit_money/settings.py
@@ -90,7 +90,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     # "ROTATE_REFRESH_TOKENS": False,
 }
