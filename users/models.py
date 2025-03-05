2b9e489a1c4d46c7dac9ca60b89714fa13e89f29
mykytaso <mykyta.soloviov@gmail.com>
2025-03-05 14:46:02 -0500

    Remove unused imports from models.py to clean up the code

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import timezone
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 
