c31cf09427d6968d03cf4fd74f946a10963a2619
mykytaso <mykyta.soloviov@gmail.com>
2025-03-06 17:14:41 -0500

    Update ALLOWED_HOSTS in settings.py to restrict access to a specific IP address

--- fire_fruit_money/settings.py
+++ fire_fruit_money/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG_VALUE", default=False)
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["54.90.220.37",]
 
 INTERNAL_IPS = ["*",]
 
