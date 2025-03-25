fd343c4a3b34a8b8262d08aaf721ee956c74ad95
mykytaso <mykyta.soloviov@gmail.com>
2025-03-25 08:33:10 -0400

    Enable token rotation for JWT authentication

--- fire_fruit_money/settings.py
+++ fire_fruit_money/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
-    # "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
 }
 
 
